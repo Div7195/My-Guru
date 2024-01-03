6fa35daa28f3457eba25b8a7e2efb815730ce2de
Abhiraj Singh <abhirajsingh05574@gmail.com>
2024-01-03 12:53:55 +0530

    ok1

--- client/src/components/profile/MentorPublicProfile.jsx
+++ client/src/components/profile/MentorPublicProfile.jsx
@@ -77,126 +77,7 @@ const [mentor, setMentor] = useState(mentorObjInitial)
     myFunction();
   }, [])
 
-//   return (
-//     <>
-//       <div style={{ display: 'flex', flexDirection: 'row' }}>
-//         <MentorSidebar />
-//         <div style={{ display: 'flex', flexDirection: 'column', width: '100%', fontFamily: 'DM Sans' }}>
-//           {/* Cover Picture */}
-//           <div style={{ height: '150px', background: '#2bedbc', position: 'relative' }}>
-//             {/* You can add cover picture here */}
-//           </div>
 
-//           <div style={{ display: 'flex', justifyContent: 'center', position: 'relative', top: '-80px' }}>
-//             {/* Circular Profile Picture */}
-//             <Avatar
-//               alt={mentor.mentorName}
-//               src={mentor.mentorImage ? mentor.mentorImage : 'https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black.png'}
-//               sx={{ width: 150, height: 150, border: '5px solid #fff' }}
-//             />
-//           </div>
-
-//           <Paper style={{ padding: '20px', marginTop: '20px' }}>
-//             <Typography variant="h5" style={{ textAlign: 'center', marginBottom: '10px' }}>
-//               {mentor.mentorName}
-//             </Typography>
-//             <Typography variant="subtitle1" style={{ color: '#555', textAlign: 'center', marginBottom: '10px' }}>
-//               Mentor ID: {id}
-//             </Typography>
-
-//             {/* Display other non-editable information */}
-//             <Typography variant="subtitle1" gutterBottom>
-//               Email: {mentor.mentorEmail}
-//             </Typography>
-//             <Typography variant="subtitle1" gutterBottom>
-//               Contact: {mentor.mentorContact}
-//             </Typography>
-//             <Typography variant="subtitle1" gutterBottom>
-//               Tagline: {mentor.mentorTagline}
-//             </Typography>
-//             {/* ... Display other non-editable information ... */}
-//             <Divider style={{ margin: '20px 0' }} />
-//             {/* Display other non-editable sections like Exams, Subjects, etc. */}
-//             <MentorEducation mentor={mentor} />
-//             <MentorExperiences mentor={mentor} />
-//             <MentorAchievements mentor={mentor} />
-//             <MentorStatistics mentor={mentor} />
-//             <MentorReviews mentor={mentor} />
-//             <MentorPlans mentor={mentor} />
-//           </Paper>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-
-//   return (
-//     <>
-//       <div style={{ display: 'flex', flexDirection: 'row' }}>
-//         <MentorSidebar />
-//         <div
-//           style={{
-//             display: 'flex',
-//             width: '100%',
-//             fontFamily: 'DM Sans',
-//             padding: '20px',
-//           }}
-//         >
-//           <Grid container spacing={3}>
-//             <Grid item xs={12} md={3}>
-//               <Paper style={{ padding: '20px', textAlign: 'center' }}>
-//                 <Avatar
-//                   alt={mentor.mentorName}
-//                   src={mentor.mentorImage ? mentor.mentorImage : 'https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black.png'}
-//                   sx={{ width: 150, height: 150 }}
-//                 />
-//                 <Typography variant="h5" style={{ marginTop: '10px' }}>
-//                   {mentor.mentorName}
-//                 </Typography>
-//                 <Typography variant="subtitle1" style={{ color: '#555', marginTop: '5px' }}>
-//                   Mentor ID: {id}
-//                 </Typography>
-//               </Paper>
-//             </Grid>
-//             <Grid item xs={12} md={9}>
-//               <Paper style={{ padding: '20px' }}>
-//                 <Typography variant="h4" gutterBottom>
-//                   Public Profile of <strong>{mentor.mentorName} </strong>
-//                 </Typography>
-//                 {/* Display other non-editable information */}
-//                 <Typography variant="subtitle1" gutterBottom>
-//                   Email: {mentor.mentorEmail}
-//                 </Typography>
-//                 <Typography variant="subtitle1" gutterBottom>
-//                   Contact: {mentor.mentorContact}
-//                 </Typography>
-//                 <Typography variant="subtitle1" gutterBottom>
-//                   Tagline: {mentor.mentorTagline}
-//                 </Typography>
-//                 {/* ... Display other non-editable information ... */}
-//                 <Divider style={{ margin: '20px 0' }} />
-//                 {/* Display other non-editable sections like Exams, Subjects, etc. */}
-//                 <MentorEducation mentor={mentor} />
-//                 <MentorExperiences mentor={mentor} />
-//                 <MentorAchievements mentor={mentor} />
-//                 <MentorStatistics mentor={mentor} />
-//                 <MentorReviews mentor={mentor} />
-//                 <MentorPlans mentor={mentor} />
-//               </Paper>
-//             </Grid>
-//           </Grid>
-//         </div>
-//       </div>
-//     </>
-//   );
-
-// //    return(
-// //     <div>
-// //         hello
-// //     </div>
-// //    )
-// };
 const [openDialog, setOpenDialog] = React.useState(false);
 
 const handleContactDetails = () => {
@@ -240,20 +121,26 @@ return (
           </Typography>
         </Paper>
 
-        {/* Education details below the cover picture */}
-        <Paper style={{ padding: '20px', marginBottom: '20px' }}>
+        {/* Education details below the cover picture 
+         <Paper style={{ padding: '20px', marginBottom: '20px' }}>
           <Typography variant="h6" gutterBottom>
             Education
           </Typography>
           <MentorEducation mentor={mentor} />
         </Paper>
 
-        {/* Display other non-editable sections like Exams, Subjects, etc. */}
-        <MentorExperiences mentor={mentor} />
+         <MentorExperiences mentor={mentor} />
         <MentorAchievements mentor={mentor} />
         <MentorStatistics mentor={mentor} />
         <MentorReviews mentor={mentor} />
-        <MentorPlans mentor={mentor} />
+        <MentorPlans mentor={mentor} /> 
+        */}
+       
+   
+
+
+
+
       </div>
 
       {/* Dialog for Contact Details */}
